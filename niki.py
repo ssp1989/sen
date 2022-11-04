f21fbb9ace538a2660f149d3ee9e38440c5d55aa
ssp1989 <117264392+ssp1989@users.noreply.github.com>
2022-11-04 12:04:20 +0530

    Update niki.py

--- niki.py
+++ niki.py
@@ -1,6 +1,11 @@
 class niki:
     def relation(self):
         print('niki is wife of sendil')
+class niki1:
+    def relation(self):
+        print('niki loves and lusts for sendil each and every secon of her life')
+        
 
 niki().relation()
+niki1().relation()
 
